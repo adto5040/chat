f7299c99e845ecb3dad543ac7a8d58126e7a9599
Adrian Tomasek <extern.adrian.tomasek@esolutions.de>
2022-02-04 11:55:18 +0100

    feat(chat): use jest for testing
    - replace karma with jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  moduleNameMapper: {
+    '@core/(.*)': '<rootDir>/src/app/core/$1'
+  },
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  transformIgnorePatterns: ['node_modules/(?!(jest-test|ngx-socket-io))'],
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  }
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { url: 'http://localhost:8988', options: {} };
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SocketIoModule.forRoot(config)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
